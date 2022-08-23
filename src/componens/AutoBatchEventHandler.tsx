2a34d2bbe0a697e48ae2664c5e799359ba6ba6cf
reachscript-jak <123ja.k123@gmail.com>
2022-08-23 15:17:58 +0900

    React17と同じAutomatic batchingの確認

---
+++ src/componens/AutoBatchEventHandler.tsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+export const AutoBatchEventHandler = () => {
+  console.log('AutoBatchEventHandler!!');
+
+  const [state1, setState1] = useState<number>(0);
+  const [state2, setState2] = useState<number>(0);
+
+  const onClickUpdateButton = () => {
+    setState1((state1) => state1 + 1);
+    setState2((state2) => state2 + 1);
+  }
+
+  return (
+    <div>
+      <p>Automatic Batching確認用（イベントハンドラ）</p>
+      <button onClick={onClickUpdateButton}>State更新</button>
+      <p>State1: {state1}</p>
+      <p>State2: {state2}</p>
+    </div>
+  )
+}
---
+++ src/componens/AutoBatchOther.tsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+type Todo = {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+export const AutoBatchOther = () => {
+  console.log('AutoBatchOther!!');
+
+  const [todos, setTodos] = useState<Todo[] | null>(null);
+  const [isFinishApi, setIsFinishApi] = useState<boolean>(false);
+
+  // Promise内等イベントハンドラ以外の場所ではAitomatic Batchingされていなかった
+  const onClickExecuteApi = () => {
+    fetch('https://jsonplaceholder.typicode.com/todos')
+      .then((res) => res.json())
+      .then((data) => {
+        setTodos(data);
+        setIsFinishApi(true);
+      })
+  }
+
+  return (
+    <div>
+      <p>Automatic Batching確認用（その他）</p>
+      <button onClick={onClickExecuteApi}>API実行</button>
+      <p>isFinishApi: {isFinishApi ? 'true' : 'false'}</p>
+      {todos?.map((todo) => <p key={todo.id}>{todo.title}</p>)}
+    </div>
+  )
+}
