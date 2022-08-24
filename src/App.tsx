99e165d6351f2887170a868d8ee1477f15c103d1
reachscript-jak <123ja.k123@gmail.com>
2022-08-24 10:57:15 +0900

    transitionが必要になるサンプル実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import './App.css';
 import { AutoBatchEventHandler } from './componens/AutoBatchEventHandler';
 import { AutoBatchOther } from './componens/AutoBatchOther';
+import { Transition } from './componens/Transition';
 
 function App() {
   return (
     <div className="App">
       <AutoBatchEventHandler />
       <AutoBatchOther />
+      <hr />
+      <Transition />
     </div>
   );
 }
---
+++ src/componens/Transition.tsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import { Avatar } from "./Avatar";
+
+type Task = {
+  id: number;
+  title: string;
+  assignee: string;
+}
+
+const member = {
+  a: 'A',
+  b: 'B',
+  c: 'C',
+}
+
+const generateDummyTasks = (): Task[] => {
+  return Array(10000).fill('').map((_, index) => {
+    const addedIndex = index + 1;
+    return {
+      id: addedIndex,
+      title: `タスク${addedIndex}`,
+      assignee: addedIndex % 3 === 0 ? member.a : addedIndex % 2 === 0 ? member.b : member.c,
+    }
+  })
+}
+const tasks = generateDummyTasks();
+
+const filteringAssignee = (assignee: string) => {
+  if (assignee === '') return tasks;
+  return tasks.filter((task) => task.assignee === assignee);
+}
+
+export const Transition = () => {
+
+  const [selectedAssignee, setSelectedAssignee] = useState<string>('');
+  const [taskList, setTaskList] = useState<Task[]>(tasks);
+
+  const onClickAssignee = (assignee: string) => {
+    setSelectedAssignee(assignee);
+    setTaskList(filteringAssignee(assignee));
+  }
+
+  return (
+    <div>
+      <p>transition</p>
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <Avatar isSelected={selectedAssignee === member.a} onClick={onClickAssignee}>{member.a}</Avatar>
+        <Avatar isSelected={selectedAssignee === member.b} onClick={onClickAssignee}>{member.b}</Avatar>
+        <Avatar isSelected={selectedAssignee === member.c} onClick={onClickAssignee}>{member.c}</Avatar>
+      </div>
+      <br />
+      <button onClick={() => onClickAssignee('')}>リセット</button>
+      {taskList.map((task) => (
+        <div key={task.id} style={{ width: '300px', margin: 'auto', background: 'lavender' }}>
+          <p>タイトル：{task.title}</p>
+          <p>担当：{task.assignee}</p>
+        </div>
+      ))}
+    </div>
+  )
+}
